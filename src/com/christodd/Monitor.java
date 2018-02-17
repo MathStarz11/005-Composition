94d3bacb20322951bed8e2e3338d101ba88e5c60
Christopher Todd <ctodd@student.yosemite.edu>
2018-02-17 00:36:22 -0800

    initial commit
    Composition tutorial for Tim Buchalka's "Java Masterclass"

---
+++ src/com/christodd/Monitor.java
@@ -0,0 +1,35 @@
+package com.christodd;
+
+public class Monitor {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResoultion;
+
+    public Monitor(String model, String manufacturer, int size, Resolution nativeResoultion) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResoultion = nativeResoultion;
+    }
+
+    public void drawPixelAt(int x, int y, String color) {
+        System.out.println("Drawing pixel at " + x + ", " + y + " in color " + color);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResoultion() {
+        return nativeResoultion;
+    }
+}
